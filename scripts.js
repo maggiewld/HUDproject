0d94ea331a525263b8abc571b4e568e7a2dd6cd2
Maggie D <45428880+maggiewld@users.noreply.github.com>
2019-02-08 12:48:10 +0000

    Test keyevent feature on video div.

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// Video --------------------------------------------
+window.addEventListener("keydown", event => {
+
+  if (event.key == "v") {
+  document.getElementById('videoDiv').style.border = "thick solid black";
+  }
+  });
+
+  window.addEventListener("keyup", event => {
+  if (event.key == "v") {
+  document.getElementById('videoDiv').style.border = "";
+  }
+});
+
+// Date ---------------------------------------------
+
+// Nav ----------------------------------------------
+
+// Status -------------------------------------------
+
+// Circular Progress Bar ----------------------------
+
+// Character Profiles -------------------------------
+
+// Welcome ------------------------------------------
