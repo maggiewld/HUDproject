959ab8b81a67a7e400e7defb7769bf49e7432543
Maggie D <45428880+maggiewld@users.noreply.github.com>
2019-02-08 13:11:19 +0000

    Add ids and code for respective keyevents for date, nav, status, char, welcome (temp style).

--- scripts.js
+++ scripts.js
@@ -13,13 +13,69 @@ window.addEventListener("keydown", event => {
 });
 
 // Date ---------------------------------------------
+window.addEventListener("keydown", event => {
 
+  if (event.key == "d") {
+  document.getElementById('dateDiv').style.border = "thick solid black";
+  }
+  });
+
+  window.addEventListener("keyup", event => {
+  if (event.key == "d") {
+  document.getElementById('dateDiv').style.border = "";
+  }
+});
 // Nav ----------------------------------------------
+window.addEventListener("keydown", event => {
 
+  if (event.key == "n") {
+  document.getElementById('navDiv').style.border = "thick solid black";
+  }
+  });
+
+  window.addEventListener("keyup", event => {
+  if (event.key == "n") {
+  document.getElementById('navDiv').style.border = "";
+  }
+});
 // Status -------------------------------------------
+window.addEventListener("keydown", event => {
 
+  if (event.key == "s") {
+  document.getElementById('statusDiv').style.border = "thick solid black";
+  }
+  });
+
+  window.addEventListener("keyup", event => {
+  if (event.key == "s") {
+  document.getElementById('statusDiv').style.border = "";
+  }
+});
 // Circular Progress Bar ----------------------------
 
 // Character Profiles -------------------------------
+window.addEventListener("keydown", event => {
 
+  if (event.key == "c") {
+  document.getElementById('charDiv').style.border = "thick solid black";
+  }
+  });
+
+  window.addEventListener("keyup", event => {
+  if (event.key == "c") {
+  document.getElementById('charDiv').style.border = "";
+  }
+});
 // Welcome ------------------------------------------
+window.addEventListener("keydown", event => {
+
+  if (event.key == "w") {
+  document.getElementById('welcomeDiv').style.border = "thick solid black";
+  }
+  });
+
+  window.addEventListener("keyup", event => {
+  if (event.key == "w") {
+  document.getElementById('welcomeDiv').style.border = "";
+  }
+});
